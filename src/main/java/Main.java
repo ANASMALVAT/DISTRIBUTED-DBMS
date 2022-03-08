792b00a251a28631de8d65601d013e6339371dbc
Parampal Singh <pr874463@dal.ca>
2022-03-07 23:41:21 -0400

    ERD and DataDump Generator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
+import datadump.datadumpCreator;
+import erd.erdCreator;
 
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        erdCreator.createERDDiagram("u1", "db1");
+        datadumpCreator.createDataDump("u1", "db1");
     }
 }
